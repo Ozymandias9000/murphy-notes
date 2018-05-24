ac3ebd3ecc8151000ec0847a259f3dfa08cc69d0
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-05-24 11:13:07 -0700

    Added stylus, nib packages and middleware handling code in server.js

--- server.js
+++ server.js
@@ -2,20 +2,39 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const stylus = require('stylus');
+const nib = require('nib');
 const { catchErrors } = require('./handlers/errorHandlers');
 const postController = require('./controllers/postController');
 require('dotenv').config({  path: 'variables.env' });
 
+// Use bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+// Use mongoose to interface with mongo 
 mongoose.connect("mongodb://localhost:27017/murphy_blog");
+
+// Use stylus & nib for CSS
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+};
+
+app.use(stylus.middleware({ 
+	src: __dirname + '/public', 
+	compile: compile
+  }
+));
 
 // Use pug for views
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+app.use(express.logger('dev'));
 
 app.use(express.static(__dirname + '/public'));
+
 
 // Routes
 app.get('/', catchErrors(postController.getposts));
