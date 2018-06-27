dbe87ac7495a9884f38a06425aac75367cb57347
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-06-27 14:09:20 -0700

    Added post validation in controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,26 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({ body: String });
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
+const { sanitizeBody } = require("express-validator/filter");
 
 exports.getposts = async (req, res) => {
-	await Post.find({}, (err, posts) => {
-		res.render('index', { posts });
-	});
+  await Post.find({}, (err, posts) => {
+    res.render("index", { posts });
+  });
 };
 
-exports.addpost = async (req, res) => {
-	if (req.body.postBody === '') {
-		res.redirect('/');
-		return;
-	}
-	const postData = new Post({
-		body: req.body.postBody
-	});
-	await postData.save();
-	res.redirect('/');
-};
+// Using express validator as middleware to trim and escape to
+// prevent cross-site scripting attacks
+exports.addpost = [
+  sanitizeBody("postBody")
+    .trim()
+    .escape(),
+
+  async (req, res) => {
+    if (req.body.postBody === "") {
+      res.redirect("/");
+      return;
+    }
+
+    const postData = new Post({
+      body: req.body.postBody
+    });
+    await postData.save();
+    res.redirect("/");
+  }
+];
 
 exports.deletepost = async (req, res) => {
-	await Post.findOneAndRemove({'_id': req.params.id});
-	res.redirect('/');
-}
+  await Post.findOneAndRemove({ _id: req.params.id });
+  res.redirect("/");
+};
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const stylus = require('stylus');
-const nib = require('nib');
-const { catchErrors } = require('./handlers/errorHandlers');
-const postController = require('./controllers/postController');
-require('dotenv').config({  path: 'variables.env' });
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const stylus = require("stylus");
+const nib = require("nib");
+const { catchErrors } = require("./handlers/errorHandlers");
+const postController = require("./controllers/postController");
+require("dotenv").config({ path: "variables.env" });
 
 // Use bodyParser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use mongoose to interface with mongo
 mongoose.connect("mongodb://localhost:27017/murphy_blog");
@@ -18,32 +18,33 @@ mongoose.connect("mongodb://localhost:27017/murphy_blog");
 // Use stylus & nib for CSS
 function compile(str, path) {
   return stylus(str)
-    .set('filename', path)
+    .set("filename", path)
     .use(nib());
-};
+}
 
-app.use(stylus.middleware({
-	src: __dirname + '/public',
-	compile: compile
-  }
-));
+app.use(
+  stylus.middleware({
+    src: __dirname + "/public",
+    compile
+  })
+);
 
 // Use pug for views
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + "/public"));
 
 // Routes
-app.get('/', catchErrors(postController.getposts));
-app.post('/addpost', postController.addpost);
-app.post('/delete/:id', catchErrors(postController.deletepost));
+app.get("/", catchErrors(postController.getposts));
+app.post("/addpost", postController.addpost);
+app.post("/delete/:id", catchErrors(postController.deletepost));
 //
 
-
 // Listen on PORT
-app.set('port', process.env.PORT || 3000);
-const server = catchErrors(app.listen(app.get('port'), () => {
-	console.log(`Server listening -> PORT 3000`);
-}));
+app.set("port", process.env.PORT || 1234);
+const server = catchErrors(
+  app.listen(app.get("port"), () => {
+    console.log(`Server listening -> PORT 1234`);
+  })
+);
